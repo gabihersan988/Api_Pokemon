d159dc4e0e8148822ee030a1f481f286cb0c1fa3
Brais Bernardez <braiseijan@gmail.com>
2024-01-08 19:35:46 +0100

    -crear function findPokemonByBame

--- main.js
+++ main.js
@@ -1,29 +1,19 @@
-let globalPokemon = null;
-function findPokemonByName() {
-  const pokemonName = prompt("Dime un pokemon");
+function findPokemonByName(event) {
+  event.preventDefault(); 
+  const inputValue = document.getElementById('buscarInput').value.toLowerCase();
+  const apiURL = 'https://pokeapi.co/api/v2/pokemon/' + inputValue;
 
-  const pokemons = fetch(
-    `https://pokeapi.co/api/v2/pokemon/?limit=1126${pokemonName}`
-  )
-    .then((resp) => resp.json())
-    .then((pokemon) => {
-      console.log(pokemon);
-      console.log(`lenght: ${pokemon.results.length}`);
-      globalPokemon = pokemon.results;
-    })
-    .catch((error) => console.error(error.message));
-
-  //return pokemon.results;
-}
-findPokemonByName();
-//console.log(`lenght: ${pokemon.results.length}`);
-
-function getLength() {
-  alert(`nombre: ${globalPokemon[0].name} url: ${globalPokemon[0].url}`);
-
-  let a = globalPokemon.filter((pokemon) => {
-    return pokemon.name == "ivysaur";
-  });
-
-  alert(`nombre: ${a[0].name} url: ${a[0].url}`);
+  fetch(apiURL)
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    cleanResultsSection();
+    printPokemonInformation(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    cleanResultsSection();
+    document.getElementById('notFound').innerText = '¿Qué? ¡Ese Pokémon no existe!'
+    });
 }
