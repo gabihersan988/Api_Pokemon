3b4279dbccd14593af4d6616fb620e9259feb871
David Molinero <davidmolinerophoto@gmail.com>
2024-01-09 19:07:22 +0100

    Añadir funcion colección pokemon en js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 //https://pokeapi.co/  -> documentación de la API
 
 //https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event#examples
+let currentPokemonInformation;
+let name;
+let imgId;
+let pokemonArray = [];
+let inputValue;
 document.getElementById('searchForm').addEventListener('submit', findPokemonByName);
 
 function findPokemonByName(event) {
   event.preventDefault(); 
-  const inputValue = document.getElementById('buscarInput').value.toLowerCase();
+  inputValue = document.getElementById('buscarInput').value.toLowerCase();
   const apiURL = 'https://pokeapi.co/api/v2/pokemon/' + inputValue;
 
   fetch(apiURL)
@@ -15,14 +20,14 @@ function findPokemonByName(event) {
   .then(data => {
     cleanResultsSection();
     printPokemonInformation(data);
+    currentPokemonInformation = data;
   })
   .catch(error => {
     console.error('Error:', error);
     cleanResultsSection();
     document.getElementById('notFound').innerText = '¿Qué? ¡Ese Pokémon no existe!'
-    });
+  });
 }
-
 function cleanResultsSection() {
   document.getElementById('notFound').innerText = '';
   document.getElementById('frontImage').removeAttribute('src');
@@ -48,18 +53,15 @@ function printPokemonInformation(pokemonInformation) {
   const ulResults = document.getElementById('listaResultados');
 
   addLiElement(ulResults, 'Nombre: ' + nombre);
-  addLiElement(ulResults, 'Altura: ' + pokemonInformation.height);
-
+  addLiElement(ulResults, 'Altura: ' + pokemonInformation.height*10 + " cm");
   addLiElement(ulResults, 'Tipos: '+ pokemonInformation.types.map(type => capitalizeFirstLetter(type.type.name)).join(', '));
-
+  addLiElement(ulResults, 'Peso: ' + pokemonInformation.weight/10 + " Kg")
+  addLiElement(ulResults, 'Puntos de vida: ' + pokemonInformation.stats[0].base_stat)
+  addLiElement(ulResults, 'Ataque: ' + pokemonInformation.stats[1].base_stat)
+  addLiElement(ulResults, 'Defensa: ' + pokemonInformation.stats[2].base_stat)
+  addLiElement(ulResults, 'Velocidad: ' + pokemonInformation.stats[5].base_stat)
   document.getElementById('resultadosSection').classList.add(pokemonInformation.types[0].type.name)
 
-  //TODO
-  console.log('Peso: ' + pokemonInformation.weight)
-  console.log('Puntos de vida: ' + pokemonInformation.stats[0].base_stat)
-  console.log('Ataque: ' + pokemonInformation.stats[1].base_stat)
-  console.log('Defensa: ' + pokemonInformation.stats[2].base_stat)
-  console.log('Velocidad: ' + pokemonInformation.stats[5].base_stat)
 }
 
 // Función para engadir elementos li a unha ul co texto recibido (Lista de características de Pokemon)
@@ -73,4 +75,42 @@ function addLiElement(ulElement, text) {
 function capitalizeFirstLetter(name) {
   let nameWithCapitalLetter = name.charAt(0).toUpperCase() + name.slice(1);
   return nameWithCapitalLetter;
+}
+const capturaPokemon = document.querySelector(".capture");
+capturaPokemon.addEventListener("click", capturaEstePokemon);
+function capturaEstePokemon () {
+  if (currentPokemonInformation) {
+    let imgElement = document.createElement('img');
+    imgElement.id = currentPokemonInformation.name;
+    imgId = imgElement.id;
+    pokedex.append(imgElement);
+    if (pokemonArray.includes(imgId)) {
+      console.log("Ya tienes este pokemon")
+      console.log(pokemonArray);
+    } else {
+      pokemonArray.push(imgId);
+      console.log(pokemonArray);
+      document.getElementById('voidP').innerText = '';
+      document.getElementById(imgId).setAttribute('class', currentPokemonInformation.name);
+      document.getElementById(imgId).setAttribute('src', currentPokemonInformation.sprites.front_default);
+      document.getElementById(imgId).setAttribute('type', "submit");
+      return pokeButton();
+    };
+    
+  } else {
+    let pElement = document.createElement("p");
+    pElement.innerText = "Aún no hay ningún Pokémon en tu colección";
+    pokedex.append(pElement);
+  };
+};
+function pokeButton() {
+  for (let i = 0; i < pokemonArray.length; i++) {
+    let setPokemon = pokemonArray[i];
+    let infoPokemonCapturado = document.querySelector("." + setPokemon);
+    console.log(infoPokemonCapturado);
+    infoPokemonCapturado.onmouseenter = () => infoPokemonCapturado.style.outline = '4px solid lightgrey';
+    infoPokemonCapturado.onmouseleave = () => infoPokemonCapturado.style.outline = '0px';
+    document.getElementById(setPokemon).addEventListener('submit', findPokemonByName);
+    inputValue = document.getElementById(setPokemon).id;
+  }
 }
